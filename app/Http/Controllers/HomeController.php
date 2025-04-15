32eafafd76827da78d45489b5d3f30cb460a7405
Ethiasdev <112874213+Ethiasdev@users.noreply.github.com>
2025-04-15 10:35:00 +0200

    Add basic dashboard statistics for admin users

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Appointment;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = auth()->user();
+        $data = [];
+        
+        if ($user && $user->isAdmin()) {
+            $data['todayAppointments'] = Appointment::whereDate('date', today())->count();
+            $data['pendingAppointments'] = Appointment::where('status', 'nieuw')->count();
+        }
+        
+        return view('home', $data);
     }
 }
